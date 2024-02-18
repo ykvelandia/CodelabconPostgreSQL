504d0139aebb7a8623569646582dcdf48bf2995b
ykvelandia <velandia416@gmail.com>
2024-02-18 17:37:56 -0500

    Mi first commit

---
+++ src/main/java/com/example/springbootPOSQL/SpringbootPosqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootPOSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPosqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootPOSQL/SpringbootPosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootPOSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
