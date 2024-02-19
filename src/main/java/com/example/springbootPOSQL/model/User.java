ba60adbe409635063ca2e8294ff60994b407ff4a
ykvelandia <velandia416@gmail.com>
2024-02-18 19:39:42 -0500

    create a user table

---
+++ src/main/java/com/example/springbootPOSQL/model/User.java
@@ -0,0 +1,43 @@
+package com.example.springbootPOSQL.model;
+
+import com.example.springbootPOSQL.dto.UserDto;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    public User(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public User() {
+    }
+
+    public User(UserDto userDto) {
+        this.name = userDto.getName();
+        this.email = userDto.getEmail();
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
